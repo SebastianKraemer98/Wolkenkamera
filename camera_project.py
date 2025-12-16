0b29478d40d8fc74fa45cb3da7edeafafbadf0ae
SebastianKraemer98 <skraemer1@hs-koblenz.de>
2025-12-16 16:56:15 +0100

    third commit

--- camera_project.py
+++ camera_project.py
@@ -1,14 +1,11 @@
-# Importieren der Bibliotheken 
 from picamera2 import Picamera2
-from datetime import datetime
+from datetime import datetime, timedelta
 import os
 import time
 
 # --- Einstellungen ---
 BASE_DIR = "/home/raspi01/Dokumente/Fernerkundung_Wolkenkamera/Wolkenbilder"
-INTERVAL_SECONDS = 30                                                              # alle 30 Sekunden ein bild
 
-# Basisverzeichnis erstellen, falls nicht vorhanden
 os.makedirs(BASE_DIR, exist_ok=True)
 
 # Kamera initialisieren
@@ -19,26 +16,35 @@ picam.start()
 
 print("Wolkenkamera gestartet. Drücke STRG+C zum Beenden.")
 
-try:
-    while True:
-        now = datetime.now()
+def sleep_until_next_full_minute():
+    now = datetime.now()
+    next_minute = (now.replace(second=0, microsecond=0) + timedelta(minutes=1))
+    seconds_to_wait = (next_minute - now).total_seconds()
+    time.sleep(seconds_to_wait)
 
-        # Tagesordner: z.B. 2025-01-24
+try:
+    # zuerst auf den nächsten Minutenbeginn synchronisieren
+    sleep_until_next_full_minute()
+
+    while True:
+        now = datetime.now()  # sollte hier praktisch immer xx:xx:00 sein
+
+        # Tagesordner
         day_folder = now.strftime("%Y-%m-%d")
         save_directory = os.path.join(BASE_DIR, day_folder)
         os.makedirs(save_directory, exist_ok=True)
 
-        # Dateiname: z.B. Wolkenbild_15-32-10.jpg
+        # Dateiname
         timestamp = now.strftime("%H-%M-%S")
-        filename = f"Wolkenbild_{timestamp}.jpg"
+        filename = f"Wolkenbild_{timestamp}.png"
         filepath = os.path.join(save_directory, filename)
 
         # Bild aufnehmen
         picam.capture_file(filepath)
         print("Gespeichert:", filepath)
 
-        # Warten bis zur nächsten Aufnahme
-        time.sleep(INTERVAL_SECONDS)
+        # bis zur nächsten vollen Minute warten
+        sleep_until_next_full_minute()
 
 except KeyboardInterrupt:
     print("\nAufnahme manuell abgebrochen.")
